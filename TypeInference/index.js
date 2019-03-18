a3ef93b99102547c696c556546f7160aa7727c06
Lucy20209060 <550502661@qq.com>
2019-03-18 11:16:58 +0800

    docs:TypeScript—类型推论

--- TypeInference/index.js
+++ TypeInference/index.js
@@ -3,3 +3,9 @@
 var myFavoriteNumber;
 myFavoriteNumber = 'seven';
 myFavoriteNumber = 7;
+window.onmousedown = function (mouseEvent) {
+    console.log(mouseEvent.button); //<- Error
+};
+// window.onmousedown = function(mouseEvent: any) {
+//     console.log(mouseEvent.button);  //<- Now, no error is given
+// };
--- TypeInference/index.ts
+++ TypeInference/index.ts
@@ -4,3 +4,11 @@
 let myFavoriteNumber;
 myFavoriteNumber = 'seven';
 myFavoriteNumber = 7;
+
+window.onkeydown = function(e) {
+    console.log(e.button);  //<- Error
+};
+
+// window.onmousedown = function(mouseEvent: any) {
+//     console.log(mouseEvent.button);  //<- Now, no error is given
+// };
